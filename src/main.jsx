79236c4cdd201254162869680b65447f4c2fc0d9
Sajag Subedi <sajagsubedi03@gmail.com>
2025-01-30 17:40:52 +0545

    Deployment: Backend url from env

--- src/main.jsx
+++ src/main.jsx
@@ -1,30 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
-import {BrowserRouter} from "react-router-dom" 
-import {ToastContainer} from "react-toastify"
+import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const client = new ApolloClient({
-	uri:"http://localhost:3000/graphql", 
-	cache: new InMemoryCache(),
-	credentials: "include",
+  uri: import.meta.env.VITE_BackendURL,
+  cache: new InMemoryCache(),
+  credentials: "include",
 });
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <ApolloProvider client={client}>
         <App />
         <ToastContainer
-            position="top-left"
-            theme="dark"
-            autoClose={3000}
-            style={{ width: "230px" }}
-          />
+          position="top-left"
+          theme="dark"
+          autoClose={3000}
+          style={{ width: "230px" }}
+        />
       </ApolloProvider>
     </BrowserRouter>
   </React.StrictMode>
-)
+);
